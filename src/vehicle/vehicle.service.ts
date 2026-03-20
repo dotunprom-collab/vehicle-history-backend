fc94af4dadbecd1239ba72d4656dec80388f7506
dotunakin <dotunprom@gmail.com>
2026-03-20 13:51:39 +0000

    fix vehicle service syntax

--- src/vehicle/vehicle.service.ts
+++ src/vehicle/vehicle.service.ts
@@ -4,37 +4,37 @@ import axios from 'axios';
 @Injectable()
 export class VehicleService {
   async getVehicle(reg: string) {
-  try {
-    const response = await axios.post(
-      'https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles',
-      {
-        registrationNumber: reg,
-      },
-      {
-        headers: {
-          'x-api-key': process.env.DVLA_API_KEY,
-          'Content-Type': 'application/json',
+    try {
+      const response = await axios.post(
+        'https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles',
+        {
+          registrationNumber: reg,
         },
-      }
-    );
+        {
+          headers: {
+            'x-api-key': process.env.DVLA_API_KEY,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
-    const data = response.data;
+      const data = response.data;
 
-    return {
-      reg,
-      make: data.make,
-      year: data.yearOfManufacture,
-      fuel: data.fuelType,
-      colour: data.colour,
-      motStatus: data.motStatus,
-    };
+      return {
+        reg,
+        make: data.make,
+        year: data.yearOfManufacture,
+        fuel: data.fuelType,
+        colour: data.colour,
+        motStatus: data.motStatus,
+      };
 
-  } catch (error: any) {
-    console.error("DVLA ERROR:", error.response?.data || error.message);
+    } catch (error: any) {
+      console.error("DVLA ERROR:", error.response?.data || error.message);
 
-    return {
-      error: "Could not fetch vehicle data",
-    };
+      return {
+        error: "Vehicle lookup failed",
+      };
+    }
   }
- }
 }
