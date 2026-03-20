9f7892d98012c14a0d958d162b81421a49919a3c
dotunakin <dotunprom@gmail.com>
2026-03-20 13:12:40 +0000

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(); // ✅ ADD THIS LINE
+  // ✅ ADD THIS LINE
+  app.enableCors({
+    origin: '*', // allow all (fine for MVP)
+  });
 
-  app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
