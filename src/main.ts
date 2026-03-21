15f761e989e397615cbcfd58f01fe77f7d4cba5b
dotunakin <dotunprom@gmail.com>
2026-03-21 04:08:05 +0000

    fix build error remove options

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,12 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
     allowedHeaders: '*',
   });
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
-  // 🔥 HANDLE PREFLIGHT MANUALLY (FINAL FIX)
-  app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.sendStatus(204);
-  });
 
   await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
