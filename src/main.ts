9163294019d231a09626c4e45d4ab21d9352bef1
dotunakin <dotunprom@gmail.com>
2026-03-21 03:48:55 +0000

    fix cors preflight error

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 🔥 FULL CORS CONFIG (IMPORTANT)
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: '*',
+    credentials: false,
   });
 
   await app.listen(process.env.PORT || 3001);
 }
-
 bootstrap();
