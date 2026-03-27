13513e002c0f83bd415c4a21aed58b260a34eee7
dotunakin <dotunprom@gmail.com>
2026-03-27 09:35:13 +0000

    remove express adapter (final clean fix)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
 async function bootstrap() {
-  const server = express();
-
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
