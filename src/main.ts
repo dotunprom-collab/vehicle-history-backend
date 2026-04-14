424e623e3237432e2be14744d46503a7da3e97f4
dotunakin <dotunprom@gmail.com>
2026-04-14 11:51:30 +0100

    fix stripe success url

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,21 @@ import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-app.enableCors({
-  origin: "*", // 🔥 allow your frontend
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-});
+  app.enableCors({
+    origin: [
+      "http://localhost:5501",
+      "http://127.0.0.1:5501"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  });
 
-  app.enableCors();
+  const port = Number(process.env.PORT) || 3001;
 
-const port = Number(process.env.PORT) || 3001;
+  await app.listen(port);
 
-await app.listen(port);
-
-console.log("🔥 SERVER IS LISTENING ON:", port);
-console.log("🔥 GLOBAL ENV:", process.env.STRIPE_SECRET_KEY);
+  console.log("🔥 SERVER IS LISTENING ON:", port);
 }
 
 bootstrap();
